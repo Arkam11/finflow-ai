f09f241507176fac2535e1859d659a991c0bde89
Arkam Mohammed <m.arkam@ymail.com>
2026-05-31 19:52:22 +0530

    fix: format jest.setup.js with prettier

--- services/gateway/jest.setup.js
+++ services/gateway/jest.setup.js
@@ -1,5 +1,5 @@
 const originalEmit = process.emit.bind(process);
-process.emit = function(event, ...args) {
+process.emit = function (event, ...args) {
   if (event === 'uncaughtException' && args[0]?.message === 'Connection is closed.') {
     return false;
   }
